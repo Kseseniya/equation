301730e01916c61c7037a85260155c7039cf75e8
Kseseniya <70349987+Kseseniya@users.noreply.github.com>
2020-09-26 15:14:54 +0300

    Update Quadratic_equation.c

--- Quadratic_equation.c
+++ Quadratic_equation.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define ALL_SOLUTIONS -1 
+#define ALL_SOLUTIONS -1
 #define NO_ROOTS 0
 #define ONE_ROOT 1
 #define TWO_ROOTS 2
@@ -13,29 +13,31 @@ int ChekingZero(double value);
 void Tests();
 
 //! Print roots of square equation
-//! 
-//! @param [in]	Num_roots	Number of roots of a square equation					
-//! @param[out]	x1			The first root
-//! @param[out] x2			The second root
-//! 
+//!
+//! @param [in]	   Num_roots	Number of roots of a square equation
+//! @param [out]	x1			The first root
+//! @param [out]    x2			The second root
+//!
 //! @note In case the equation has one root, the output is x1.
 //! @note In case the equation has no roots, the output is "No roots".
 //! @note In case of infinite number of roots, the output is "Solutions are all numbers".
-//! 
+//!
 //! @attention If the coefficients are entered incorrectly, the output is "Error"
-							
+
 int main()
 {
 	Tests();
 
-	printf("Enter a,b,c: ");
-
+	printf("Enter a, b, c : \n");
 	double a = 0, b = 0, c = 0;
 
-	if (scanf_s("%lg  %lg  %lg", &a, &b, &c) != 3)
+	int check = scanf("%lg %lg %lg", &a, &b, &c);
+
+	while (check != 3)
 	{
-		printf("Error\n");
-		return 0;
+		fflush(stdin);
+		printf("Sorry, cannot understand you, please try again\n");
+		check = scanf("%lg %lg %lg", &a, &b, &c);
 	}
 
 	double x1 = 0, x2 = 0;
@@ -44,7 +46,7 @@ int main()
 
 	assert(isfinite(x1));
 	assert(isfinite(x2));
-	 
+
 	switch (Num_roots)
 	{
 	case NO_ROOTS:
@@ -56,10 +58,10 @@ int main()
 	case TWO_ROOTS:
 		printf("x1 = %lg, x2 = %lg \n", x1, x2);
 		break;
-	case ALL_SOLUTIONS:	
+	case ALL_SOLUTIONS:
 		printf("Solutions are all numbers\n");
 		break;
-	default: 
+	default:
 		printf("main() : ERROR: Num_roots = %d\n ", Num_roots);
 		break;
 	}
@@ -67,17 +69,17 @@ int main()
 }
 
 //! Solves a square equation
-//! 
-//! @param [in]		a	a-coefficient  
+//!
+//! @param [in]		a	a-coefficient
 //! @param [in]		b	b-coefficient
 //! @param [in]		c	c-coefficient
 //! @param [out]	x1	x1-Pointer to the 1st root
 //! @param [out]	x2	x2-Pointer to the 2nd root
-//! 
+//!
 //! @return Number of roots
-//! 
+//!
 //! @note	In case of infinite number of roots
-//!			returns ALL_SOLUTIONS 
+//!			returns ALL_SOLUTIONS
 
 int Solve_equation(double a, double b, double c, double* x1, double* x2)
 {
@@ -105,6 +107,7 @@ int Solve_equation(double a, double b, double c, double* x1, double* x2)
 		else
 		{
 			*x1 = (-c) / b;
+			ChekingZero(*x1);
 			return ONE_ROOT;
 		}
 	}
@@ -114,7 +117,7 @@ int Solve_equation(double a, double b, double c, double* x1, double* x2)
 		if (ChekingZero(d))
 		{
 			*x1 = *x2 = -b / (2 * a);
-
+			ChekingZero(*x1);
 			return ONE_ROOT;
 		}
 		else
@@ -137,50 +140,48 @@ int Solve_equation(double a, double b, double c, double* x1, double* x2)
 //! This function allows to compare values against zero
 //!
 //! @param[in] value   The value of the variable to be compared
-//! 
+//!
 //! @return 1 - if the condition is met, 0 - if the condition is not met
 
-int ChekingZero (double value)
-	{
+int ChekingZero(double value)
+{
 	return(fabs(value) <= 1e-5);
-	}
+}
 
 //!This function allows to check correctness of the program
 
 void Tests()
 {
 	FILE* in = fopen("in.txt", "r");;
-	FILE* out =  fopen("out.txt", "w");;
+	FILE* out = fopen("out.txt", "w");;
 
 	double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+	int Checking = 0, Num_test = 1, Coeff = 0;
 
-	while (fscanf(in, "%lg %lg %lg", &a, &b, &c) > 0)
+	while ((Coeff = fscanf(in, "%lg%lg%lg%d", &a, &b, &c, &Checking)) > 0)
 	{
-		int Num_roots = Solve_equation(a, b, c, &x1, &x2);
-
-		assert(isfinite(x1));
-		assert(isfinite(x2));
-
-		switch (Num_roots)
+		if (Coeff != 4)
 		{
-		case 0:
-			fprintf(out, "No roots\n");
-			break;
-		case 1:
-			fprintf(out, "x = %lg\n", x1);
-			break;
-		case 2:
-			fprintf(out, "x1 = %lg  ", x1);
-			fprintf(out, "x2 = %lg\n", x2);
-			break;
-		case ALL_SOLUTIONS:
-			fprintf(out, "Solutions are all numbers\n");
-			break;
-		default:
-			fprintf(out, "main() : ERROR: Num_roots = %d\n ", Num_roots);
-			break;
+			fprintf(out, "Error, not enough variables");
+		}
+		else
+		{
+			int Num_roots = Solve_equation(a, b, c, &x1, &x2);
+
+			assert(isfinite(x1));
+			assert(isfinite(x2));
+			if (Num_roots == Checking)
+			{
+				fprintf(out, "Test #%d OK\n", Num_test);
+			}
+			else
+			{
+				fprintf(out, "Test #%d FAIL. Expected : %d. Program return : %d \n", Num_test, Checking, Num_roots);
+			}
+			Num_test++;
 		}
 	}
 	fclose(in);
 	fclose(out);
+	return;
 }
